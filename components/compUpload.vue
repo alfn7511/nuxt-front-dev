64493d4c269dd391efb9becb5805fe377969e4d7
inhye.kim <alfn7511@gmail.com>
2023-02-27 10:17:52 +0900

    init

---
+++ components/compUpload.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- counter -->
+  <v-file-input
+    chips
+    multiple
+    show-size
+    label="첨부파일업로드"
+    placeholder="파일을 선택해주세요."
+    variant="outlined"
+    clearable
+    hide-details
+  />
+</template>
+<script>
+export default {
+  name: 'CompUpload',
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <compUpload />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
